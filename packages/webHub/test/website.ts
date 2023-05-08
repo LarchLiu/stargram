891151d911923317e060775d1f63abe7c07d0b12
Alex.Liu <larch.liu@gmail.com>
2023-05-08 12:05:57 +0800

    fix(core): webHub test

--- packages/webHub/test/website.ts
+++ packages/webHub/test/website.ts
@@ -1,30 +1,8 @@
-import type { FetchWebsite, LoaderUrls } from '@starnexus/core'
-import { getDomain } from '@starnexus/core'
+import { getWebsiteInfo as getInfo } from '../../core/src/website'
 import { routes } from '../src/routes-auto-generate'
 
-export async function getWebsiteInfo(urls: LoaderUrls, header?: Record<string, string>): Promise<FetchWebsite> {
-  let info: FetchWebsite = { error: 'StarNexus error: Not supported website.' }
-  const domain = getDomain(urls.webUrl)
-
-  if (routes[domain]) {
-    const router = routes[domain]
-    if (router.paths) {
-      for (let i = 0; i < router.paths.length; i++) {
-        const pathInfo = router.paths[i]
-        const loaderUrls = pathInfo.filter(urls)
-        if (loaderUrls) {
-          info = await pathInfo.loader(loaderUrls, header)
-          if (info.data) {
-            info.data.meta.domain = domain
-            info.data.meta.website = router.name
-          }
-
-          return info
-        }
-      }
-    }
-    return { error: `${router.name} error: Not supported website.` }
-  }
+export async function getWebsiteInfo(url: string) {
+  const info = await getInfo({ webUrl: url }, routes)
 
   return info
 }
