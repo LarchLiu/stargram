21727bedaf7ccc43947d580c56ae6a2f7558cb13
LarchLiu <LarchLiu@users.noreply.github.com>
2023-06-05 09:21:48 +0000

    chore: release v0.0.1-beta.0

--- app/bot/telegram-cl/dist/index.js
+++ app/bot/telegram-cl/dist/index.js
@@ -46,9 +46,9 @@ const a = {
   // 检查更新的分支
   UPDATE_BRANCH: "main",
   // 当前版本
-  BUILD_TIMESTAMP: "1685956841",
+  BUILD_TIMESTAMP: "1685956864",
   // 当前版本 commit id
-  BUILD_VERSION: "1fef12f",
+  BUILD_VERSION: "e3b4c8b",
   /**
   * @type {I18n}
   */
