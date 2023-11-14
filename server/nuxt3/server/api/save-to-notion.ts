26fee62d1a424bbd1069c8cdda83ad187a8d0023
Alex.Liu <larch.liu@gmail.com>
2023-11-14 18:13:18 +0800

    fix(nuxt3): get apiKey from headers

--- server/nuxt3/server/api/save-to-notion.ts
+++ server/nuxt3/server/api/save-to-notion.ts
@@ -2,11 +2,12 @@ import { NotionDataStorage } from '@stargram/core/storage/notion'
 import { errorMessage } from '@stargram/core/utils'
 
 export default eventHandler(async (event) => {
+  let apiKey = getHeader(event, 'Authorization')
+  apiKey = apiKey!.split('Bearer ')[1]
   const req = await readBody<{ title: string; url: string; summary: string; apiKey: string; databaseId: string; image: string }>(event)
   const title = req.title
   const url = req.url
   const summary = req.summary
-  const apiKey = req.apiKey
   const databaseId = req.databaseId
   const defaultOgImage = req.image
 
