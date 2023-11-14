aa3404443b35b283819210447283b7d1db7c7b2f
Alex.Liu <larch.liu@gmail.com>
2023-11-14 18:52:34 +0800

    fix(nuxt3): no image url

--- server/nuxt3/server/api/save-to-notion.ts
+++ server/nuxt3/server/api/save-to-notion.ts
@@ -4,15 +4,15 @@ import { errorMessage } from '@stargram/core/utils'
 export default eventHandler(async (event) => {
   let apiKey = getHeader(event, 'Authorization')
   apiKey = apiKey!.split('Bearer ')[1]
-  const req = await readBody<{ title: string; url: string; summary: string; apiKey: string; databaseId: string; image: string }>(event)
+  const req = await readBody<{ title: string; url: string; summary: string; apiKey: string; databaseId: string }>(event)
   const title = req.title
   const url = req.url
   const summary = req.summary
   const databaseId = req.databaseId
-  const defaultOgImage = req.image
+  // const defaultOgImage = req.image
 
   try {
-    const storage = new NotionDataStorage({ apiKey, databaseId, defaultOgImage },
+    const storage = new NotionDataStorage({ apiKey, databaseId, defaultOgImage: '' },
       { title, summary, url, content: '', categories: [], meta: {} })
     const data = await storage.create()
     return data
