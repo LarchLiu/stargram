3ec8a236fc11ca364aa594615105c70a09f80e90
Alex.Liu <larch.liu@gmail.com>
2024-01-17 18:55:29 +0800

    chore: app name

--- server/nuxt3/constants/index.ts
+++ server/nuxt3/constants/index.ts
@@ -1,7 +1,7 @@
 import { Cryption } from '@stargram/core/utils'
 
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Stargram'
+export const appDescription = 'Manage all your Starred Pages'
 export const DEFAULT_OG_IMAGE = 'https://kiafhufrshqyrvlpsdqg.supabase.co/storage/v1/object/public/pics-bed/stargram.png?v=stargramogimage'
 export const DEFAULT_STARGRAM_HUB = 'https://stargram.cc'
 export const C1 = 26739
